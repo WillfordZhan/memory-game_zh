20ee4a1ba24c4d38da8611c189b9de540e03e17f
Willford <1506676573@qq.com>
2018-01-29 18:22:42 +0800

    feature: implement the func for match

--- js/app.js
+++ js/app.js
@@ -9,25 +9,28 @@ var Game = function(icons,deckElem){
 
 // Initializing the Game 
 Game.prototype.init = function(){
-    
     var self = this;
     var time = 0;
     var moves = 0;
-    var open = [];
-    self.icons.map(function(){
-        var card = new card();
-        card.elem.addEventListener(function(){
-            
+    var opens = [];
+    self.icons.map(function(icon){
+        var card = new card(icon);
+        card.elem.addEventListener('click',function(){
+            card.check();
         });
     });
 };
 
+Game.prototype.showResult = function(){
+    // Show result after game is over
+};
+
 Game.prototype.restart = function(){
-    // restart the Game
+    // Restart the Game
 };
 
 Game.prototype.getScore = function(){
-    // generate score according to time and moves
+    // Generate score according to time and moves
 };
 
 
@@ -37,6 +40,7 @@ var card = function(icon){
     var elem = generateElem();
 };
 
+// Generate the HTMML element of the card 
 card.prototype.generateElem = function(){
     var liElem = document.createElement('li');
     liElem.className = 'card';
@@ -46,13 +50,27 @@ card.prototype.generateElem = function(){
     return liElem;
 };
 
-card.prototype.isMatch = function(){
-    // Judge whether 2 cards match
-    return true;
+// Judge whether 2 cards match
+card.prototype.isMatch = function(icon){
+    if (opens[length - 1] == icon) {
+        return true;
+    }
+    return false;
 };
 
-card.prototype.open = function(){
-    
+card.prototype.check = function(){
+    this.elem.className = "card open show";
+    opens.push(this.icon);
+    if (opens.length % 2 == 0) {
+        if (!isMatch(this.icon)) {
+            this.elem.className = "card";
+            // TODO: checkout the false match animation
+        }
+        else{
+            opens.pop();
+            opens.pop();
+        }
+    }
 };
 
 
@@ -81,9 +99,9 @@ function shuffle(array) {
 /*
  * 设置一张卡片的事件监听器。 如果该卡片被点击：
  *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
- *  - 将卡片添加到状态为 “open” 的 *数组* 中（将这个功能放在你从这个函数中调用的另一个函数中）
+ *  - 将卡片添加到状态为 “opens” 的 *数组* 中（将这个功能放在你从这个函数中调用的另一个函数中）
  *  - 如果数组中已有另一张卡，请检查两张卡片是否匹配
- *    + 如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 如果卡片匹配，将卡片锁定为 "opens" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
